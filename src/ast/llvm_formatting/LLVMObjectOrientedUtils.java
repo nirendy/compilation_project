f7eff2c5eedc3e11f3ab2af47a6c61186be51d3f
Nimrod Pansky <nimrod@optibus.com>
2020-12-04 21:37:53 +0200

    EX 2. LLVMObjectOrientedUtils - implemented

--- src/ast/llvm_formatting/LLVMObjectOrientedUtils.java
+++ src/ast/llvm_formatting/LLVMObjectOrientedUtils.java
@@ -1,46 +1,135 @@
 package ast.llvm_formatting;
 
-import ast.AstType;
-import ast.IntAstType;
+import ast.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LLVMObjectOrientedUtils {
+
+    Map<String, Map<String, MethodData>> classToMethodsMapping;
+    Map<String, Map<String, VarDecl>> classesToFieldsMapping;
+
+    private static class MethodData {
+        final int index;
+        final AstType returnType;
+        final List<AstType> formalArgsTypes;
+
+        private MethodData(int index, MethodDecl methodNode) {
+            this.index = index;
+            this.returnType = methodNode.returnType();
+
+            List<AstType> formalArgsTypes = new ArrayList<>();
+            for (FormalArg formalArg : methodNode.formals()) {
+                formalArgsTypes.add(formalArg.type());
+            }
+            this.formalArgsTypes = formalArgsTypes;
+        }
+    }
+
+    public LLVMObjectOrientedUtils(Program program) {
+        classToMethodsMapping = createClassToMethodsMapping(program);
+        classesToFieldsMapping = createClassToFieldsMapping(program);
+    }
+
     public int getFieldOffset(String className, String fieldName) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return 888;
+        return typeToSize(classesToFieldsMapping.get(className).get(fieldName).type());
     }
 
     public AstType getFieldType(String className, String fieldName) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return new IntAstType();
+        return classesToFieldsMapping.get(className).get(fieldName).type();
     }
 
     public int getMethodIndex(String className, String methodName) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return 555;
+        return classToMethodsMapping.get(className).get(methodName).index;
     }
 
     public AstType getMethodReturnType(String className, String methodName) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return new IntAstType();
+        return classToMethodsMapping.get(className).get(methodName).returnType;
     }
 
     public List<AstType> getMethodFormalArgsTypes(String className, String methodName) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        ArrayList<AstType> formalArgsTypes = new ArrayList<>();
-        formalArgsTypes.add(new IntAstType());
-        return formalArgsTypes;
+        return classToMethodsMapping.get(className).get(methodName).formalArgsTypes;
     }
 
     public int getInstanceSize(String className) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return 666;
+        // Each class instance is using the first 8 bits for the V-table
+        int size = 8;
+
+        Map<String, VarDecl> classFields = classesToFieldsMapping.get(className);
+        for (VarDecl field : classFields.values()) {
+            size += typeToSize(field.type());
+        }
+
+        return size;
     }
 
     public int getNumberOfMethods(String className) {
-        // TODO: implement (waiting for @nimrod.pansky)
-        return 333;
+        return classToMethodsMapping.get(className).size();
     }
+
+    private int typeToSize(AstType type) {
+        if (type instanceof IntAstType)
+            return 32;
+        else if (type instanceof BoolAstType)
+            return 1;
+
+        // The type is of a reference
+        return 8;
+    }
+
+    private Map<String, Map<String, MethodData>> createClassToMethodsMapping(Program program) {
+        /* Creating a mapping between each class in the program to its methods, while taking inheritance into account.
+        * Each method has a saved index, which is consistent throughout inheritance */
+        Map<String, Map<String, MethodData>> classToMethodsMapping = new HashMap<>();
+
+        Map<String, MethodData> classMethods;
+        int index;
+        for (ClassDecl classNode : program.classDecls()) {
+            classMethods = new HashMap<>();
+
+            if (classNode.superName() != null) {
+                // The class has a super class, hence its inheriting its methods
+                classMethods.putAll(classToMethodsMapping.get(classNode.superName()));
+            }
+
+            index = classMethods.size();
+            for (MethodDecl methodNode : classNode.methoddecls()) {
+                if (!classMethods.containsKey(methodNode.name())) {
+                    // A method can override an inherited class. In that case it shouldn't be re-added to the list
+                    classMethods.put(methodNode.name(), new MethodData(index, methodNode));
+                    index++;
+                }
+            }
+            classToMethodsMapping.put(classNode.name(), classMethods);
+        }
+
+        return classToMethodsMapping;
+    }
+
+    private Map<String, Map<String, VarDecl>> createClassToFieldsMapping(Program program) {
+        /* Creating a mapping between each class in the program to its fields, while taking inheritance into account. */
+        Map<String, Map<String, VarDecl>> classToFieldsMapping = new HashMap<>();
+
+        Map<String, VarDecl> classFields;
+        for (ClassDecl classNode : program.classDecls()) {
+            classFields = new HashMap<>();
+
+            if (classNode.superName() != null) {
+                // The class has a super class, hence its inheriting its fields
+                classFields.putAll(classToFieldsMapping.get(classNode.superName()));
+            }
+
+            for (VarDecl field : classNode.fields()) {
+                classFields.put(field.name(), field);
+            }
+
+            classToFieldsMapping.put(classNode.name(), classFields);
+        }
+
+        return classToFieldsMapping;
+    }
+
 }
